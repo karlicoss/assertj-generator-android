3661f0b9c8770d9a9a6012accc917033514f47ff
Dima Gerasimov <karlicos@yandex-team.ru>
2016-04-28 19:43:10 +0300

    Demo of simple entity and test using assertj generator

---
+++ assertj-generator-android-demo/src/main/java/com/github/karlicoss/asssertjgenerator/demo/entities/User.java
@@ -0,0 +1,30 @@
+package com.github.karlicoss.asssertjgenerator.demo.entities;
+
+import java.util.List;
+
+public class User {
+
+    private final String name;
+
+    private final boolean premium;
+
+    private final List<String> permissions;
+
+    public User(String name, boolean premium, List<String> permissions) {
+        this.name = name;
+        this.premium = premium;
+        this.permissions = permissions;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public boolean isPremium() {
+        return premium;
+    }
+
+    public List<String> getPermissions() {
+        return permissions;
+    }
+}
---
+++ assertj-generator-android-demo/src/test/java/com/github/karlicoss/asssertjgenerator/demo/entities/UserTest.java
@@ -0,0 +1,23 @@
+package com.github.karlicoss.asssertjgenerator.demo.entities;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static java.util.Arrays.asList;
+
+public class UserTest {
+
+    private User user;
+
+    @Before
+    public void beforeEachTest() {
+        user = new User("Dmitrii", true, asList("read", "write"));
+    }
+
+    @Test
+    public void assertionsGeneratorDemo() {
+        UserAssert.assertThat(user).hasName("Dmitrii");
+        UserAssert.assertThat(user).isPremium();
+        UserAssert.assertThat(user).hasPermissions("read");
+    }
+}
